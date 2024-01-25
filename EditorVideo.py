54ed0303406a1e90451e51e60646c537816020cb
quynv <quy.ngovan>
2024-01-25 08:47:19 +0700

    update fix bug

--- EditorVideo.py
+++ EditorVideo.py
@@ -45,24 +45,41 @@ def chen_video(input_file, background_file, output_file):
     cmd_get_info = ['ffprobe', '-v', 'error', '-show_entries', 'stream=width,height', '-of', 'csv=p=0', background_file]
     result = subprocess.run(cmd_get_info, stdout=subprocess.PIPE, text=True)
     width, height = map(int, result.stdout.strip().split(','))
-
+    new_width2 = 426/720 * 4/3 * height
+    new_height2 = 3/4 * height
     # Xác định kích thước của video được chèn và tính toán vị trí để đặt video vào trung tâm
     cmd_get_info_chen = ['ffprobe', '-v', 'error', '-show_entries', 'stream=width,height', '-of', 'csv=p=0', input_file]
     result_chen = subprocess.run(cmd_get_info_chen, stdout=subprocess.PIPE, text=True)
     width_chen, height_chen = map(int, result_chen.stdout.strip().split(','))
-    
+    new_width = 853/720 * 4/3 * height_chen
+    new_height =  3/4 * height_chen
+    crop_x = (width_chen - new_width) // 2
+    crop_y = (height_chen - new_height) // 2
+    print(new_width, new_height, crop_x, crop_y)
     # Xác định thời lượng của video chèn
     cmd_get_duration = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'csv=p=0', input_file]
     result_duration = subprocess.run(cmd_get_duration, stdout=subprocess.PIPE, text=True)
-    duration_chen = float(result_duration.stdout.strip())
-    repeat_duration = 60 - duration_chen
-    filtergraph = (
-        f'[1:v]scale=320:576,eq=brightness={brightness_factor},setpts={speed_factor}*PTS[chen];'
-        f'[2:v]scale=320:192,eq=brightness={brightness_factor_background},format=gray,setpts={speed_factor}*PTS[video_to_add];'
-        f'[0:v]scale=320:384,eq=brightness={brightness_factor},[chen]overlay=0:0[outv];'
-        f'[outv][video_to_add]overlay=0:384[final_outv]'
-    )
+    duration_source = float(result_duration.stdout.strip())
 
+    # Xác định thời lượng của video chèn
+    cmd_get_duration = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'csv=p=0', background_file]
+    result_duration = subprocess.run(cmd_get_duration, stdout=subprocess.PIPE, text=True)
+    duration_chen = float(result_duration.stdout.strip())
+
+    filtergraph = (
+        f'[1:v]scale=720:1280,eq=brightness={brightness_factor},setpts={speed_factor}*PTS[chen];'
+        f'[2:v]crop={new_width2}:{new_height2}:(in_w-{new_width2})/2:(in_h-{new_height2})/2,scale=720:426,eq=brightness={brightness_factor_background},format=yuva420p,colorchannelmixer=aa=0.5,setpts={speed_factor}*PTS[video_to_add];'
+        f'[0:v]crop={new_width}:{new_height}:(in_w-{new_width})/2:(in_h-{new_height})/2, scale=720:853, eq=brightness={brightness_factor},[chen]overlay=0:0[outv];'
+        f'[outv][video_to_add]overlay=0:853[final_outv]'
+    )
+    if duration_source > duration_chen:
+        filtergraph = (
+        f'[1:v]scale=720:1280,eq=brightness={brightness_factor},setpts={speed_factor}*PTS[chen];'
+        f'[2:v]loop={(duration_source - duration_chen)/duration_chen}:32767:0,crop={new_width2}:{new_height2}:(in_w-{new_width2})/2:(in_h-{new_height2})/2,scale=720:426,eq=brightness={brightness_factor_background},format=yuva420p,colorchannelmixer=aa=0.5,setpts={speed_factor}*PTS[video_to_add];'
+        f'[0:v]crop={new_width}:{new_height}:(in_w-{new_width})/2:(in_h-{new_height})/2, scale=720:853, eq=brightness={brightness_factor},[chen]overlay=0:0[outv];'
+        f'[outv][video_to_add]overlay=0:853[final_outv]'
+    )    
+    #,scale=320:384:flags=lanczos
     cmd = [
         'ffmpeg',
         '-i', input_file,
@@ -98,10 +115,8 @@ def convert_to_grayscale(input_video, output_video_grayscale):
     # Chạy lệnh FFmpeg
     subprocess.run(cmd, check=True) 
     
-# if __name__ == '__main__':
-#     #edit_video("videos/hehe.mp4", "hehe_save.mp4", cuts)
-#     #flip_video("videos/hehe.mp4", "hehe_save.mp4")
-#     chen_video("video-sources/hehe.mp4", "video-sources/hehe.mp4", "hehe_save.mp4")
+if __name__ == '__main__':
+    #edit_video("videos/hehe.mp4", "hehe_save.mp4", cuts)
+    #flip_video("videos/hehe.mp4", "hehe_save.mp4")
+    chen_video("video-sources/lebaobinh.mp4", "video-sources/background.mp4", "hehe_save.mp4")
     #convert_to_grayscale("videos/waves.mp4", "videos/background.mp4")
-    # bitrate = get_video_bitrate("videos/Moustaphacoryva05mh34zen2uxm/7247511524326067482.mp4")
-    # print(bitrate)
